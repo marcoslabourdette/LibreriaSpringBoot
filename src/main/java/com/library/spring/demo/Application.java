5cb6823a63dce849f2d01a650012ed20205bffa5
Marcos Labourdette <marcoslabourdette@gmail.com>
2024-06-26 12:08:18 -0300

    deploy 3

--- src/main/java/com/library/spring/demo/Application.java
+++ src/main/java/com/library/spring/demo/Application.java
@@ -2,11 +2,28 @@ package com.library.spring.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    @Configuration
+    public static class MyConfiguration {
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+                }
+            };
+        }
     }
 }
